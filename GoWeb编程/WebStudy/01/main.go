2a6f59a0bf3274177f1b9cefaac2d5a1edc48186
Tohsaka_evil <1477717283@qq.com>
2022-07-17 23:57:14 +0800

    GoWebStudy

---
+++ GoWeb编程/WebStudy/01/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"reflect"
+	"runtime"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello!")
+}
+func log(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
+		fmt.Println("Handler function called -" + name)
+		h(w, r)
+	}
+}
+func main() {
+	http.Handle("/", log(hello))
+	http.ListenAndServe(":8080", nil)
+}
