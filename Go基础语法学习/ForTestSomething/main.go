5bb0a70e7ee12a18ee22e2776722d5fcf84364a0
Tohsaka_evil <1477717283@qq.com>
2022-07-24 17:04:07 +0800

    for test something

--- Go基础语法学习/ForTestSomething/main.go
+++ Go基础语法学习/ForTestSomething/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main(){
+import "fmt"
 
+func update_slice(arr []int) []int {
+	fmt.Println("Update Before....")
+	fmt.Printf("%v,%p\n", arr, arr)
+	arr = append(arr, 10)
+	fmt.Println("Update After....")
+	fmt.Printf("%v,%p\n", arr, arr)
+	return arr
+}
+func main() {
+	arr := make([]int, 0, 1)
+	arr = append(arr, 5)
+	fmt.Println("Use Function Before...")
+	fmt.Printf("%v,%p\n", arr, arr)
+	arr = update_slice(arr)
+	fmt.Println("Use Function After...")
+	fmt.Printf("%v,%p\n", arr, arr)
 }
